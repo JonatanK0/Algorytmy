be4ce40601a8545ee467622c4b8a9a7943fe3c43
JonatanK0 <154448037+Adam7721@users.noreply.github.com>
2024-01-08 16:01:05 +0100

    xd

---
+++ random.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ 
+void randomF(int wyl_liczby[], int n) {
+    for (int i = 0; i < n; i++) {
+        int random = rand() % 20 + 2;
+        wyl_liczby[i] = random;
+        cout << wyl_liczby[i] << " ";
+    }
+}
+ 
+int main() {
+    int n = 12;
+    int wyl_liczby[n];
+ 
+    randomF(wyl_liczby, n);
+    cout << endl;
+ 
+    return 0;
+}
