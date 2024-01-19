f97bb126527791ee122091b04f86f98e57714b74
JonatanK0 <kozlowskijonatancs@gmail.com>
2024-01-19 15:36:19 +0100

    new files

---
+++ SortowaniePrzezScalanie.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+ 
+using namespace std;
+ 
+const int MAX_ROZMIAR = 6;
+ 
+void wypelnijTab(int dane[], int rozmiar) {
+    cout << "Podaj " << rozmiar << " liczb do tablicy:" << endl;
+    for (int i = 0; i < rozmiar; ++i) {
+        cout << "Liczba " << i + 1 << ": ";
+        cin >> dane[i];
+    }
+}
+ 
+void drukujTab(int dane[], int rozmiar) {
+    for_each(dane, dane + rozmiar, [](int x){ cout << x << " "; });
+    cout << endl;
+}
+ 
+void sortowaniePrzezScalanie(int dane[], int poczatek, int koniec) {
+    if (poczatek < koniec) {
+        int srodek = poczatek + (koniec - poczatek) / 2;
+        sortowaniePrzezScalanie(dane, poczatek, srodek);
+        sortowaniePrzezScalanie(dane, srodek + 1, koniec);
+        inplace_merge(dane + poczatek, dane + srodek + 1, dane + koniec + 1);
+    }
+}
+ 
+int main() {
+    int dane[MAX_ROZMIAR];
+    int rozmiar = MAX_ROZMIAR;
+ 
+    wypelnijTab(dane, rozmiar);
+    cout << "Tablica przed sortowaniem przez scalanie:" << endl;
+    drukujTab(dane, rozmiar);
+ 
+    sortowaniePrzezScalanie(dane, 0, rozmiar - 1);
+ 
+    cout << "Tablica po sortowaniu przez scalanie:" << endl;
+    drukujTab(dane, rozmiar);
+ 
+    return 0;
+}
