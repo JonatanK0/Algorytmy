1a227736ee73c0a478ac72f14099427790fd7f29
JonatanK0 <155749833+JonatanK0@users.noreply.github.com>
2024-02-11 15:35:33 +0100

    Update main.cpp

--- Algorytmy sortowania /Sortowanie przez wstawianie /main.cpp
+++ Algorytmy sortowania /Sortowanie przez wstawianie /main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include "insertionSort.hpp"
+
+int main() {
+    const int size = 6;
+    int arr[size] = {5, 2, 9, 1, 5, 6};
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    insertionSort(arr, size);
+
+    std::cout << "Sorted array (insertion sort): ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
 
