b8d0dcc7688ff76417d8a2449ea7918aee9707bb
JonatanK0 <155749833+JonatanK0@users.noreply.github.com>
2024-02-11 15:33:15 +0100

    Create quickSort.cpp

---
+++ Algorytmy sortowania /Sortowanie szybkie/quickSort.cpp
@@ -0,0 +1,25 @@
+#include "quickSort.hpp"
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; ++j) {
+        if (arr[j] <= pivot) {
+            ++i;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+}
