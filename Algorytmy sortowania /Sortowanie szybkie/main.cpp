e47f0544748de1ac98154d56edc01ae35631fc85
JonatanK0 <155749833+JonatanK0@users.noreply.github.com>
2024-02-11 15:31:46 +0100

    Update main.cpp

--- Algorytmy sortowania /Sortowanie szybkie/main.cpp
+++ Algorytmy sortowania /Sortowanie szybkie/main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include "quickSort.hpp"
+
+int main() {
+    const int size = 6;
+    int arr[size] = {5, 2, 9, 1, 5, 6};
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    quickSort(arr, size);
+
+    std::cout << "Sorted array (quick sort): ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
 
