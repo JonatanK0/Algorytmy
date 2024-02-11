b08e890e65b7991212b24da2c777a4d6b30432d0
JonatanK0 <155749833+JonatanK0@users.noreply.github.com>
2024-02-11 15:27:53 +0100

    Update main.cpp

--- Algorytmy sortowania /Sortowanie przez scalanie /main.cpp
+++ Algorytmy sortowania /Sortowanie przez scalanie /main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include "mergeSort.hpp"
 
+int main() {
+    const int size = 6;
+    int arr[size] = {5, 2, 9, 1, 5, 6};
+
+    std::cout << "Original array: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    mergeSort(arr, size);
+
+    std::cout << "Sorted array (merge sort): ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
