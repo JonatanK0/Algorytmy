6594b2a72a365322adf1a7805656e52bdac13da6
JonatanK0 <155749833+JonatanK0@users.noreply.github.com>
2024-02-14 20:19:55 +0100

    Create deklaracje.hpp

---
+++ Algorytmy wyszukiwania /deklaracje.hpp
@@ -0,0 +1,6 @@
+int wyszukiwanie_linowe(const int *t, int size, int key);
+int wyszukiwanie_linoweZWartownikiem(const int *t, int size, int key);
+int wyszukiwanie_skokowe(const int *t, int size, int key);
+int wyszukiwanie_binarne(const int *t, int size, int key);
+int wyszukiwanie_interpolacyjne(const int *t, int size, int key);
+
